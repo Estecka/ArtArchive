6455090a70f12d8742d419f1aef6b0273f7a4804
Estecka <estecka@galacsys.com>
2019-02-13 22:32:29 +0100

    Form remembers entered values upon submiting

--- templates/artworkForm.php
+++ templates/artworkForm.php
@@ -1,22 +1,23 @@
+<?php include("shorthands.php") ?>
 <div>
-	<form action="">
+	<form action="" method="post">
 		<label for="slug">Slug</label> 
-		<input id="slug" name="slug" type="text"/>
+		<input id="slug" name="slug" type="text" value="<?=value($_POST['slug'])?>"/>
 		
 		<br/>
 		
 		<label for="title">Title</label> 
-		<input id="title" name="title" type="text"/>
+		<input id="title" name="title" type="text" value="<?=value($_POST['title'])?>"/>
 
 		<br/>
 
 		<label for="date">Date</label> 
-		<input id="date" name="date" type="date">
+		<input id="date" name="date" type="date"  value="<?=value($_POST['date'])?>"/>
 
 		<br/>
 		
 		<label for="description">Descriptions</label> <br/>
-		<textarea id="description" name="description" ></textarea>
+		<textarea id="description" name="description"><?=value($_POST['description'])?></textarea>
 
 		<br/>
 		
