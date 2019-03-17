503dfa9319bde3e52054fbb7892bf3954856d5f4
Estecka <estecka@galacsys.com>
2019-03-17 02:23:19 +0100

    ArtForm can now add and remove tags from artworks

--- site/art/update.php
+++ site/art/update.php
@@ -8,19 +8,30 @@ if (empty($slug)) {
 }
 
 $artwork = ArtworkDTO::CreateFrom($_POST);
+$keep = isset($_POST["keep"]) ? array_keys($_POST["keep"]) : array();
+$add =  isset($_POST["add"])  ? array_keys($_POST["add"])  : false;
 $bdd = new DBService();
 
 try {
+	$bdd->StartTransaction();
 	$response = $bdd->UpdateArtwork($slug, $artwork);
-	if ($response)
-		header("Location:".URL::Artwork($artwork->slug));
-	else {
+	if (!$response) {
+		$bdd->Rollback();
 		PageBuilder::ErrorDocument(404);
 		die;
 	}
+	else {
+		$slug = $artwork->slug;
+		$bdd->RemoveTagsFromArtwork($slug, $keep, true);
+		if ($add)
+			$bdd->AddTagsToArtwork($slug, $add, true);
+		$bdd->CommitTransaction();
+	}
+	header("Location:".URL::Artwork($artwork->slug));
 } catch (PDOException $e){
 	print($e->getCode());
 	print("<br/>");
 	print($e->getMessage());
+	$bdd->Rollback();
 }
 ?>
