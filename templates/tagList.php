7068feb2ed1a558fdec863864ab3e7ad3377d333
Estecka <estecka@galacsys.com>
2019-04-13 21:30:32 +0200

    Empty categories will appear on the tags page anyway.

--- templates/tagList.php
+++ templates/tagList.php
@@ -14,13 +14,17 @@ foreach($tags as $tag)
 
 
 
-foreach($cats as $cat) if(!empty($cat->tags))  {
+foreach($cats as $cat) {
 	$h3 = $cat->GetName();
 	if ($cat->slug != null){
 		$url = URL::Category($cat->slug);
 		$h3 = "<a href=\"$url\">$h3</a>";
 	}
 	print("<h3>$h3</h3>");
+
+	if(empty($cat->tags))
+		print("This category is empty");
+	else
 	foreach($cat->tags as $tag) {
 		$style = empty($cat->color) ? null : "style ='color: $cat->color'";
 		?>
