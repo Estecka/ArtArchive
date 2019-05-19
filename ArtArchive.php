b0dbdbdb4dc97601432b4f6152da277d48f2df2c
Estecka <estecka@galacsys.com>
2019-05-19 21:12:03 +0200

    Moved the URL class into its own file

--- ArtArchive.php
+++ ArtArchive.php
@@ -1,6 +1,7 @@
 <?php
 define("__ROOT__", __DIR__);
 require_once("shorthands.php");
+require_once("Url.php");
 require_once("database/DBService.php");
 require_once("templates/PageBuilder.php");
 
@@ -21,65 +22,4 @@ ArtArchive::$settings = ArtArchive::$database->GetSettings(
 		"SiteName" => "MyArtDump",
 	)
 );
-
-class URL {
-	static public function Home() : string {
-		return "/site/";
-	}
-	static public function SiteSettings() : string {
-		return "/site/webmaster/settings.php";
-	}
-
-	/** REGION ARTWORKS */
-	static public function Artwork(string $slug) : string {
-		return "/site/art/?art=$slug";
-	}
-	static public function EditArt(string $slug) : string {
-		return "/site/art/edit.php?art=$slug";
-	}
-	static public function SubmitArt() : string {
-		return "/site/submit/art/";
-	}
-
-	static public function DeleteArt(string $slug) : string {
-		return "/site/art/delete.php?art=$slug";
-	}
-	static public function Search(string $tags = null, $page = 0) : string {
-		$url = "/site/search.php";
-		if ($tags){
-			$url .= "?tags=$tags";
-			if ($page)
-				$url .= "&page=$page";
-		}
-		return $url;
-	}
-
-	/** REGION TAGS */
-	static public function TagsHome() : string {
-		return "/site/tags.php";
-	}
-	static public function Tag(string $slug) : string {
-		return "/site/tag/?tag=$slug";
-	}
-	static public function EditTag(string $slug) : string {
-		return "/site/tag/edit.php?tag=$slug";
-	}
-	static public function SubmitTag() : string {
-		return "/site/submit/tag/";
-	}
-
-	/** REGION CATEGORIES */
-	static public function OrderCategory() : string {
-		return "/site/categories.php";
-	}
-	static public function Category(string $slug) : string {
-		return "/site/category/?category=$slug";
-	}
-	static public function EditCategory(string $slug) : string {
-		return "/site/category/edit?category=$slug";
-	}
-	static public function SubmitCategory() : string {
-		return "/site/submit/category/";
-	}
-}
 ?>
---
+++ Url.php
@@ -0,0 +1,62 @@
+<?php
+class URL {
+	static public function Home() : string {
+		return "/site/";
+	}
+	static public function SiteSettings() : string {
+		return "/site/webmaster/settings.php";
+	}
+
+	/** REGION ARTWORKS */
+	static public function Artwork(string $slug) : string {
+		return "/site/art/?art=$slug";
+	}
+	static public function EditArt(string $slug) : string {
+		return "/site/art/edit.php?art=$slug";
+	}
+	static public function SubmitArt() : string {
+		return "/site/submit/art/";
+	}
+
+	static public function DeleteArt(string $slug) : string {
+		return "/site/art/delete.php?art=$slug";
+	}
+	static public function Search(string $tags = null, $page = 0) : string {
+		$url = "/site/search.php";
+		if ($tags){
+			$url .= "?tags=$tags";
+			if ($page)
+				$url .= "&page=$page";
+		}
+		return $url;
+	}
+
+	/** REGION TAGS */
+	static public function TagsHome() : string {
+		return "/site/tags.php";
+	}
+	static public function Tag(string $slug) : string {
+		return "/site/tag/?tag=$slug";
+	}
+	static public function EditTag(string $slug) : string {
+		return "/site/tag/edit.php?tag=$slug";
+	}
+	static public function SubmitTag() : string {
+		return "/site/submit/tag/";
+	}
+
+	/** REGION CATEGORIES */
+	static public function OrderCategory() : string {
+		return "/site/categories.php";
+	}
+	static public function Category(string $slug) : string {
+		return "/site/category/?category=$slug";
+	}
+	static public function EditCategory(string $slug) : string {
+		return "/site/category/edit?category=$slug";
+	}
+	static public function SubmitCategory() : string {
+		return "/site/submit/category/";
+	}
+}
+?>
