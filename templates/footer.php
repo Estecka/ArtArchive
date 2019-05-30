c4d1f9ff6a19be29088f37421f9441b3a85bfaa3
Estecka <estecka@galacsys.com>
2019-05-30 19:11:13 +0200

    Added © 2019 Estecka to the footer.

--- templates/footer.php
+++ templates/footer.php
@@ -1,2 +1,2 @@
 <hr>
-Powered by <a href="<?=URL::SourceCode()?>">MyArtDump</a> v<?=ArtArchive::$version?>
+Powered by <a href="<?=URL::SourceCode()?>">MyArtDump</a> v<?=ArtArchive::$version?> © 2019 Estecka
