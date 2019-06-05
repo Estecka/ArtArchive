a7827d9c71b63d6b3d68ca9222e92be6c7517351
Estecka <estecka@galacsys.com>
2019-06-05 23:23:15 +0200

    Gave art cards a somewhat stylish look.

--- templates/ArtCard.php
+++ templates/ArtCard.php
@@ -2,7 +2,10 @@
 /** @var ArtWorkDTO */
 $name = $art->title ?? $art->slug;
 ?>
-<div>
-	<a href="<?=URL::Artwork($art->slug)?>"><h3><?=$name?></h3></a>
+<a href="<?=URL::Artwork($art->slug)?>"><div class="card">
+	<div class="viewport">
+		<img/>
+	</div>
+	<h4 class="row"><?=$name?></h4>
 	<?=$art->date?>
-</div>
+</div></a>
