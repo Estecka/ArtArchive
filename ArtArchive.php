23fb472938b1da3b9e88f95fdacde97c454b3eda
Estecka <estecka@galacsys.com>
2019-12-14 22:32:17 +0100

    Incremented version to 0.2.1

--- ArtArchive.php
+++ ArtArchive.php
@@ -7,7 +7,7 @@ require_once("templates/PageBuilder.php");
 require_once "auth/authenticator.php";
 
 class ArtArchive {
-	static $version = "0.2.1-indev";
+	static $version = "0.2.1";
 
 	/** @var array */
 	static $settings;
