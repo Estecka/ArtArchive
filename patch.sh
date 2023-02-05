e935dbe968bd46f5c6bf0df7f3724ba49c8ece39
Estecka <estecka@galacsys.com>
2023-02-05 15:25:50 +0100

    Versions 0.6.1
    Patch.sh now outputs diff.

--- patch.sh
+++ patch.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+git diff $1 $2 --name-status --no-renames
 git archive $2 -o patch.zip $(git diff $1 $2 --name-only --no-renames --diff-filter=MA)
 git diff $1 $2 --name-only --no-renames --diff-filter=D
